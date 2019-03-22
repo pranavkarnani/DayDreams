7f0e7a7cf8a20bcd38539fb014486257e8c55b66
Pranav Karnani <pranavkarnani@icloud.com>
2019-03-23 01:20:23 +0530

    Initial Commit

---
+++ Autism/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autism
+//
+//  Created by Pranav Karnani on 23/03/19.
+//  Copyright © 2019 Pranav Karnani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
